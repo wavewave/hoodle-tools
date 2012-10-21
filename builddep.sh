3d3340028c208b7b9dfe36d922df4a04c5b35e49
Ian-Woo Kim <ianwookim@gmail.com>
2012-10-21 18:24:49 -0400

    travis-ci integration

---
+++ builddep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+cabal install transformers
+
+ghc-pkg list
+
+mkdir deps
+git clone https://github.com/wavewave/devadmin.git deps/devadmin
+cd deps/devadmin ; cabal install --force-reinstalls ; cd ../../
+$HOME/.cabal/bin/build cloneall --config=build.conf
+
+cabal install gtk2hs-buildtools
+$HOME/.cabal/bin/build bootstrap --config=build.conf
+
+cabal install --enable-tests
+
