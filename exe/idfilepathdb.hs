38b040b545f68b1d36b2e308a6d48f93d2a0cfc1
Ian-Woo Kim <ianwookim@gmail.com>
2013-10-25 07:44:01 -0400

    implement dbdiff

--- exe/idfilepathdb.hs
+++ exe/idfilepathdb.hs
@@ -10,7 +10,8 @@ import           Data.Attoparsec
 import qualified Data.ByteString.Char8 as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.Map as M
-import           Data.Maybe (mapMaybe)
+import           Data.Maybe (mapMaybe, isNothing, fromJust)
+-- import           Data.Foldable 
 import           Data.Time.Clock
 import           Data.Time.Clock.POSIX
 import           Data.Digest.Pure.MD5
@@ -28,15 +29,18 @@ import System.Console.CmdArgs
 import Data.Hoodle.Simple
 -- 
 import Text.Hoodle.Parse.Attoparsec 
--- import qualified Text.Hoodle.Migrate.V0_1_999_to_V0_1_9999 as MV
--- import Text.Hoodle.Parse.Conduit 
 import Graphics.Hoodle.Render
 import Graphics.Hoodle.Render.Type
 import Graphics.Rendering.Cairo 
+-- 
+
+
+
 
 data IdFilePathDB = AllFiles { hoodlehome :: FilePath }
                   | SingleFile { hoodlehome :: FilePath 
                                , singlefilename :: FilePath } 
+                  | DBDiff
                   deriving (Show,Data,Typeable)
 
 allfiles :: IdFilePathDB 
@@ -48,23 +52,26 @@ singlefile =
   SingleFile { hoodlehome = def &= typ "HOODLEHOME" &= argPos 0 
              , singlefilename = def &= typ "FILEPATH" &= argPos 1
              }
-
+  
+dbdiff :: IdFilePathDB 
+dbdiff = DBDiff
+  
 mode :: IdFilePathDB
-mode = modes [allfiles, singlefile] 
+mode = modes [allfiles, singlefile, dbdiff] 
 
 main :: IO () 
 main = do 
   params <- cmdArgs mode
   case params of 
-    AllFiles hdir -> allfilework hdir 
+    AllFiles hdir      -> allfilework hdir 
     SingleFile hdir fp -> singlefilework hdir fp 
+    DBDiff             -> dbdiffwork
   
-  
+allfilework :: FilePath -> IO ()
 allfilework hdir = do 
   homedir <- getHomeDirectory 
   r <- readProcess "find" [homedir </> "Dropbox" </> "hoodle","-name","*.hdl","-print"] "" 
   mapM_ (singlefilework hdir) (lines r)
-
 
 splitfunc :: String -> (String,(String,String))
 splitfunc str = 
@@ -73,30 +80,65 @@ splitfunc str =
       str3 = read (tail rest2)
   in (str1,(str2,str3))
 
+
+dbdiffwork :: IO ()
+dbdiffwork = do 
+  homedir <- getHomeDirectory 
+  tmpdir <- getTemporaryDirectory 
+  let newdbfile = homedir </> "Dropbox" </> "hoodleiddb.dat"
+      olddbfile = homedir </> "Dropbox" </> "hoodleiddb.dat.old"
+   
+  newdbstr <- readFile newdbfile 
+  olddbstr <- readFile olddbfile
+  let makedb = M.fromList . map splitfunc . lines 
+      (newdb,olddb) = (makedb newdbstr, makedb olddbstr) 
+     
+  mapM_ print (M.toList (checkdiff olddb newdb))     
+  -- print newdb 
+  -- print olddb 
+  
+  
+data DiffType = Same 
+              | RightNew (String,FilePath) 
+              | LeftNew (String,FilePath) 
+              | Conflict (String,FilePath) (String,FilePath)  
+              deriving (Show,Eq) 
+
+checkdiff :: M.Map String (String,FilePath) 
+          -> M.Map String (String,FilePath) 
+          -> M.Map String DiffType
+checkdiff olddb newdb = 
+  let oks = M.keys olddb 
+      nks = M.keys newdb
+      ks = oks ++ nks
+      rightnew k = (k,(RightNew . fromJust) (M.lookup k newdb))
+      compf k = case (M.lookup k olddb,M.lookup k newdb) of  
+        (Nothing,Nothing) -> Nothing
+        (Just v,Nothing) -> Just (k,LeftNew v)
+        (Nothing,Just v) -> Just (k,RightNew v)
+        (Just v,Just v') -> 
+          if v == v' then Just (k,Same) else Just (k,Conflict v v')
+  in (M.fromList . filter ((/=Same).snd) . mapMaybe compf) ks
+
+singlefilework :: FilePath -> FilePath -> IO ()
 singlefilework hdir oldfp = do 
   homedir <- getHomeDirectory 
   tmpdir <- getTemporaryDirectory 
-  -- r <- readProcess "find" [hdir,"-name","*.hdl","-print"] "" 
   let origdbfile = homedir </> "Dropbox" </> "hoodleiddb.dat"
       tmpfile = tmpdir </> "hoodleiddb.dat"
   copyFile origdbfile tmpfile 
-  
   fp <- makeRelative hdir <$> canonicalizePath oldfp 
   str <- readFile tmpfile 
   let assoclst = (map splitfunc . lines) str 
       assocmap = M.fromList assoclst 
-  
-  let replacefunc n _ = Just n 
-
+      replacefunc n _ = Just n 
   muuid <- checkHoodleIdMd5 oldfp 
   let nmap = case muuid of 
                Nothing -> assocmap 
                Just (uuid,md5str) -> M.alter (replacefunc (md5str,fp)) uuid assocmap
       nstr = (unlines . map (\(x,(y,z))->x ++ " " ++ y ++ " " ++ show z) . M.toList) nmap 
- 
   writeFile origdbfile nstr 
   removeFile tmpfile 
-
 
 checkHoodleIdMd5 :: FilePath -> IO (Maybe (String,String))
 checkHoodleIdMd5 fp = do 
@@ -109,26 +151,7 @@ checkHoodleIdMd5 fp = do
           md5str = show (md5 (L.fromChunks [bstr]))
       return (Just (idstr,md5str))
       
-{- 
--- |  
-onefile :: Handle -> FilePath -> IO ()   
-onefile fh fp  = do 
-  putStrLn $ fp ++ " is now being analyzed " 
-  muuidmd5 <- checkHoodleIdMd5 fp 
-  {- bstr <- B.readFile fp
-  eh <- checkVersionAndGetIfHigherVersion bstr
-  case eh of 
-    Left str -> print str -}
-  case muuidmd5 of 
-    Nothing -> return () 
-    Just (idstr,md5str) -> do 
-      fs <- getFileStatus fp 
-      let etime = modificationTime fs
-          utctime = posixSecondsToUTCTime (realToFrac etime)
-      hPutStrLn fh ( idstr ++  " " ++ md5str ++ " " ++ show fp )
-  -}    
 
-     
 -- | using attoparsec without any built-in xml support 
 checkVersionAndGetIfHigherVersion :: B.ByteString -> IO (Either String Hoodle) 
 checkVersionAndGetIfHigherVersion bstr = do 
@@ -136,7 +159,7 @@ checkVersionAndGetIfHigherVersion bstr = do
     Left str -> return (Left str )
     Right v -> do 
       if ( v < "0.1.9999" ) 
-        then return (Left "low version") -- MV.migrate bstr
+        then return (Left "low version") 
         else return (parseOnly hoodle bstr)
 
    
